ca8e74bba85d29120019ba8f380dfaf8e2c35857
AshpreetD365 <121854263+AshpreetD365@users.noreply.github.com>
2023-01-03 15:50:29 +0530

    Update changereq.js
    Assesment Approved status,createdon,description

--- changereq.js
+++ changereq.js
@@ -1,35 +1,95 @@
-var parameters = {};
-var target = {};
-target.opportunityid = "FC446033-0911-EA11-A811-000D3AB4DE89"; //Delete if creating new record 
-target["@odata.type"] = "Microsoft.Dynamics.CRM.opportunity";
-parameters.Target = target;
-
-var retrieveRecordChangeHistoryRequest = {
-    Target: parameters.Target,
-
-    getMetadata: function() {
-        return {
-            boundParameter: null,
-            parameterTypes: {
-                "Target": {
-                    "typeName": "mscrm.crmbaseentity",
-                    "structuralProperty": 5
-                }
-            },
-            operationType: 1,
-            operationName: "RetrieveRecordChangeHistory"
-        };
-    }
-};
-
-Xrm.WebApi.online.execute(retrieveRecordChangeHistoryRequest).then(
-    function success(result) {
-        if (result.ok) {
-            var results = JSON.parse(result.responseText);
-            console.log(results);
-        }
-    },
-    function(error) {
-        Xrm.Utility.alertDialog(error.message);
-    }
-);
+<ImportExportXml version="9.2.22111.206" SolutionPackageVersion="9.2" languagecode="1033" generatedBy="CrmLive" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <SolutionManifest>
+    <UniqueName>DummyTest</UniqueName>
+    <LocalizedNames>
+      <LocalizedName description="DummyTest" languagecode="1033" />
+    </LocalizedNames>
+    <Descriptions />
+    <Version>1.0.0.0</Version>
+    <Managed>0</Managed>
+    <Publisher>
+      <UniqueName>Morgan_Stanley_COE</UniqueName>
+      <LocalizedNames>
+        <LocalizedName description="Morgan Stanley COE" languagecode="1033" />
+      </LocalizedNames>
+      <Descriptions>
+        <Description description="This publisher is for Morgan Stanley COE specific configuration and development. " languagecode="1033" />
+      </Descriptions>
+      <EMailAddress xsi:nil="true"></EMailAddress>
+      <SupportingWebsiteUrl xsi:nil="true"></SupportingWebsiteUrl>
+      <CustomizationPrefix>mscoe</CustomizationPrefix>
+      <CustomizationOptionValuePrefix>40675</CustomizationOptionValuePrefix>
+      <Addresses>
+        <Address>
+          <AddressNumber>1</AddressNumber>
+          <AddressTypeCode>1</AddressTypeCode>
+          <City xsi:nil="true"></City>
+          <County xsi:nil="true"></County>
+          <Country xsi:nil="true"></Country>
+          <Fax xsi:nil="true"></Fax>
+          <FreightTermsCode xsi:nil="true"></FreightTermsCode>
+          <ImportSequenceNumber xsi:nil="true"></ImportSequenceNumber>
+          <Latitude xsi:nil="true"></Latitude>
+          <Line1 xsi:nil="true"></Line1>
+          <Line2 xsi:nil="true"></Line2>
+          <Line3 xsi:nil="true"></Line3>
+          <Longitude xsi:nil="true"></Longitude>
+          <Name xsi:nil="true"></Name>
+          <PostalCode xsi:nil="true"></PostalCode>
+          <PostOfficeBox xsi:nil="true"></PostOfficeBox>
+          <PrimaryContactName xsi:nil="true"></PrimaryContactName>
+          <ShippingMethodCode>1</ShippingMethodCode>
+          <StateOrProvince xsi:nil="true"></StateOrProvince>
+          <Telephone1 xsi:nil="true"></Telephone1>
+          <Telephone2 xsi:nil="true"></Telephone2>
+          <Telephone3 xsi:nil="true"></Telephone3>
+          <TimeZoneRuleVersionNumber xsi:nil="true"></TimeZoneRuleVersionNumber>
+          <UPSZone xsi:nil="true"></UPSZone>
+          <UTCOffset xsi:nil="true"></UTCOffset>
+          <UTCConversionTimeZoneCode xsi:nil="true"></UTCConversionTimeZoneCode>
+        </Address>
+        <Address>
+          <AddressNumber>2</AddressNumber>
+          <AddressTypeCode>1</AddressTypeCode>
+          <City xsi:nil="true"></City>
+          <County xsi:nil="true"></County>
+          <Country xsi:nil="true"></Country>
+          <Fax xsi:nil="true"></Fax>
+          <FreightTermsCode xsi:nil="true"></FreightTermsCode>
+          <ImportSequenceNumber xsi:nil="true"></ImportSequenceNumber>
+          <Latitude xsi:nil="true"></Latitude>
+          <Line1 xsi:nil="true"></Line1>
+          <Line2 xsi:nil="true"></Line2>
+          <Line3 xsi:nil="true"></Line3>
+          <Longitude xsi:nil="true"></Longitude>
+          <Name xsi:nil="true"></Name>
+          <PostalCode xsi:nil="true"></PostalCode>
+          <PostOfficeBox xsi:nil="true"></PostOfficeBox>
+          <PrimaryContactName xsi:nil="true"></PrimaryContactName>
+          <ShippingMethodCode>1</ShippingMethodCode>
+          <StateOrProvince xsi:nil="true"></StateOrProvince>
+          <Telephone1 xsi:nil="true"></Telephone1>
+          <Telephone2 xsi:nil="true"></Telephone2>
+          <Telephone3 xsi:nil="true"></Telephone3>
+          <TimeZoneRuleVersionNumber xsi:nil="true"></TimeZoneRuleVersionNumber>
+          <UPSZone xsi:nil="true"></UPSZone>
+          <UTCOffset xsi:nil="true"></UTCOffset>
+          <UTCConversionTimeZoneCode xsi:nil="true"></UTCConversionTimeZoneCode>
+        </Address>
+      </Addresses>
+    </Publisher>
+    <RootComponents>
+      <RootComponent type="1" schemaName="mcapcoe_assessment" behavior="2" />
+    </RootComponents>
+    <MissingDependencies>
+      <MissingDependency>
+        <Required type="1" schemaName="mcapcoe_assessment" displayName="Assessment" solution="Active" />
+        <Dependent type="1" schemaName="mcapcoe_assessment" displayName="Assessment" />
+      </MissingDependency>
+      <MissingDependency>
+        <Required type="9" schemaName="mcapcoe_mcapapprovalstatus" displayName="MCAP Approval Status" solution="Active" />
+        <Dependent type="2" schemaName="mcapcoe_mcapapprovalstatus" displayName="Approval Status" parentSchemaName="mcapcoe_assessment" parentDisplayName="Assessment" />
+      </MissingDependency>
+    </MissingDependencies>
+  </SolutionManifest>
+</ImportExportXml>
